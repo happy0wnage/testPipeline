09cde15f3371bf77d45c5c6f55664c411d770466
Jenkins <jenkins-mgf@dsi.io>
2020-03-10 05:58:05 -0700

    Bumps version to 2.1.12

--- mgf/src/main/java/com/blackstar/utils/Version.java
+++ mgf/src/main/java/com/blackstar/utils/Version.java
@@ -1,5 +1,5 @@
 package com.blackstar.utils;
 
 public class Version {
-    public static final String APPLICATION_VERSION = "2.0.36";
+    public static final String APPLICATION_VERSION = "2.1.12";
 }
